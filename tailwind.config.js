a48d8a472769c761c36053ae7a42db44ec0e7411
Nouaaman <nouaamank1@gmail.com>
2024-11-05 11:17:56 +0100

    add custom colors and font families to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B23EFF",
+        secondary: "#FDC500",
+        lightYellow: "#FFE352",
+        darkPurple: "#1F0034",
+        black: "#0E0E0E",
+        bg: "#161622",
+        white: "#eee",
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 };
