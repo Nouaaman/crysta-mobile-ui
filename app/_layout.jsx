5271b52d1fb24088d49d7725db2c76e2b283280a
Nouaaman <nouaamank1@gmail.com>
2024-11-05 12:35:51 +0100

    rename Index component to Welcome, update Stack.Screen name, and change splash background color

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,14 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 
 import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="home" />
+            <Stack.Screen name="index" />
         </Stack>
     );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Index() {
+export default function Welcome() {
     return (
         <View>
             <Text className='color-orange-600'>index</Text>
