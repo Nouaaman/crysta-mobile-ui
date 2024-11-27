03e8527fe493b326356ec0c8984f0ae66ae0e7be
Nouaaman <nouaamank1@gmail.com>
2024-11-27 22:40:19 +0100

    refactor: integrate Node.js support in useUpscale hook and add sample project files

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,28 @@ const {
 
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = wrapWithReanimatedMetroConfig(withNativeWind(config, { input: "./global.css" }));
+// Modify the resolver to blacklist nodejs-assets, android, and ios folders
+config.resolver.blacklistRE = blacklist([
+    /\/nodejs-assets\/.*/, // Exclude nodejs-assets folder
+    /\/android\/.*/,       // Exclude android folder
+    /\/ios\/.*/,           // Exclude ios folder
+]);
+
+module.exports = wrapWithReanimatedMetroConfig(
+    withNativeWind(config, { input: "./global.css" })
+);
+
+// const {
+//     wrapWithReanimatedMetroConfig,
+// } = require('react-native-reanimated/metro-config');
+
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = wrapWithReanimatedMetroConfig(withNativeWind(config, { input: "./global.css" }));
