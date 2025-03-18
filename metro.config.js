19771af5d31c792a6f40698af48be6a31cbf4459
Nouaaman <nouaaman.dev@gmail.com>
2025-03-18 21:52:11 +0100

    Remove upscale feature and update dependencies
    Here are the changes made in this commit:
    - Removed useUpscale hook and related node configs
    - Added camera and GL dependencies
    - Adjusted metro config and bundleIdentifier
    - Fixed duplicate permission in app.json

--- metro.config.js
+++ metro.config.js
@@ -1,31 +1,22 @@
 const {
     wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
+} = require("react-native-reanimated/metro-config");
 
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const blacklist = require('metro-config/src/defaults/exclusionList');
+const blacklist = require("metro-config/src/defaults/exclusionList");
 
 const config = getDefaultConfig(__dirname);
 
+//add .bin for models
+config.resolver.sourceExts = [...config.resolver.sourceExts, "bin"];
+
 // Modify the resolver to blacklist nodejs-assets, android, and ios folders
 config.resolver.blacklistRE = blacklist([
-    /\/nodejs-assets\/.*/, // Exclude nodejs-assets folder
-    /\/android\/.*/,       // Exclude android folder
-    /\/ios\/.*/,           // Exclude ios folder
+    /\/android\/.*/, // Exclude android folder
+    /\/ios\/.*/, // Exclude ios folder
 ]);
 
-module.exports = wrapWithReanimatedMetroConfig(
-    withNativeWind(config, { input: "./global.css" })
-);
+const configWithNativeWind = withNativeWind(config, { input: "./global.css" });
 
-// const {
-//     wrapWithReanimatedMetroConfig,
-// } = require('react-native-reanimated/metro-config');
-
-// const { getDefaultConfig } = require("expo/metro-config");
-// const { withNativeWind } = require("nativewind/metro");
-
-// const config = getDefaultConfig(__dirname);
-
-// module.exports = wrapWithReanimatedMetroConfig(withNativeWind(config, { input: "./global.css" }));
+module.exports = wrapWithReanimatedMetroConfig(configWithNativeWind);
